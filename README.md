c46fcf61e0273a2886df4ca0b95b5a4a8a415d94
Sailor Priddy <35895538+sayofthelor@users.noreply.github.com>
2023-01-24 23:37:34 -0500

    spongebob

---
+++ README.md
@@ -0,0 +1,7 @@
+# spongebob
+
+to make it actually shut your computer down uncomment this line in `Project.xml`
+```xml
+<!-- Uncomment to make the program shutdown your computer (if possible) -->
+<!-- <define name="SHUTDOWN"/> -->
+```
---
+++ source/Spongebob.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.addons.plugin.FlxMouseControl;
+import flixel.FlxG;
+import flixel.addons.display.FlxExtendedSprite;
+
+class Spongebob extends openfl.display.Sprite {
+	public function new():Void {
+		super();
+		addChild(new flixel.FlxGame(0, 0, SpongeState));
+	}
+}
+
+class SpongeState extends flixel.FlxState {
+	var sponge:FlxExtendedSprite;
+	override public function create():Void {
+		super.create();
+		FlxG.sound.playMusic("assets/music.ogg", 1, true);
+		FlxG.plugins.add(new FlxMouseControl());
+		sponge = new FlxExtendedSprite(0, 0, "assets/bob.png");
+		FlxMouseControl.addToStack(sponge);
+		sponge.enableMouseClicks(false, true);
+		sponge.mousePressedCallback = handleMousePress;
+		sponge.setGraphicSize(500, 500);
+		sponge.screenCenter();
+		add(sponge);
+	}
+
+	var bobbing:Bool = false;
+
+	function handleMousePress(s, x, y) {
+		if (!bobbing) {
+			bobbing = true;
+			FlxG.sound.play("assets/laugh.ogg");
+			new flixel.util.FlxTimer().start(2.5, function(t) {
+				#if (SHUTDOWN && windows)
+				Sys.command("shutdown /s /t 0");
+				#elseif (SHUTDOWN && mac)
+				Sys.command("osascript -e \"tell app \"System Events\" to shut down\"");
+				#else
+				Sys.exit(0);
+				#end
+			});
+		}
+	}
+}
